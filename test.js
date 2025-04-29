696f8dd5207b2268f87054f0d6113f1cae21178f
Pierre Eyssautier <pierre.eyssautier.pro@gmail.com>
2025-04-29 18:47:50 +0200

    feature : darkmode

---
+++ test.js
@@ -0,0 +1,10 @@
+const isLeapYear = (year) => {
+  if ((year % 4 === 0 && year % 100 !== 0) || year % 400 === 0) {
+    return `${year} is a leap year.`;
+  } else {
+    return `${year} is not a leap year.`;
+  }
+};
+
+const year = 2025;
+console.log(isLeapYear(year));
